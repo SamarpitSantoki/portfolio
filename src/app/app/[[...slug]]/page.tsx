9189de6d5bbc22b030c0eb732692cfdc277a24ba
SamarpitSantoki <samarpit.santoki@gmail.com>
2024-11-20 12:08:17 +0530

    feat: some changes

--- src/app/app/[[...slug]]/page.tsx
+++ src/app/app/[[...slug]]/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+"use client";
+import { useSearchParams } from "next/navigation";
 
-function page() {
-  return <div>you are in the app</div>;
+function Page() {
+  const searchParams = useSearchParams();
+
+  const openApp = () => {
+    window.location.href = `exp+worldclasslearningapp://${searchParams.get(
+      "path"
+    )}`;
+  };
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <button
+        onClick={openApp}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Open in App
+      </button>
+    </div>
+  );
 }
 
-export default page;
+export default Page;
